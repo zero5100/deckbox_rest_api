b5579709faaf1625c0e49150e9c6dbf839568e89
selevanm <selevanm@gmail.com>
2012-02-03 23:07:59 -0600

    YUI stubbed out

---
+++ public/widgets/testWidgets/fetchData/fetchData.client.js
@@ -0,0 +1,15 @@
+feather.ns("deckbox_rest_api");
+(function() {
+  deckbox-rest-api.testWidgets = feather.Widget.create({
+    name: "deckbox_rest_api.fetchData",
+    path: "widgets/testWidgets/fetchData/",
+    prototype: {
+      onInit: function() {
+        
+      },
+      onReady: function() {
+        
+      }
+    }
+  });
+})();
